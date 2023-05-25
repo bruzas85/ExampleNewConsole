bb38bbfed35b5a1c59e4d0fdee7a2063ccf223de
Erik Bruzhas <bruzas.85@mail.ru>
2023-05-26 00:08:11 +0300

    сумма элементов массива

---
+++ example_Arrays/Program.cs
@@ -0,0 +1,11 @@
+﻿
+// Находим сумму элементов массива
+int[] arr = { 6, 4, 3, 9, 12, 2, 8 };
+int i = 0;
+int sumNumbers = 0;
+while (i < arr.Length)
+{
+    sumNumbers += arr[i];
+    i++;
+}
+Console.Write($"Сумма элементов массива равна: {sumNumbers}");
