0067c8fb2439ecd32fc7902c1e4c428cd3425be2
Erik Bruzhas <bruzas.85@mail.ru>
2023-05-26 18:52:27 +0300

    Попарная сумма элементов массива

--- example_Arrays/Program.cs
+++ example_Arrays/Program.cs
@@ -1,11 +1,22 @@
 ﻿
 // Находим сумму элементов массива
-int[] arr = { 6, 4, 3, 9, 12, 2, 8 };
-int i = 0;
-int sumNumbers = 0;
-while (i < arr.Length)
+// int[] arr = { 6, 4, 3, 9, 12, 2, 8 };
+// int i = 0;
+// int sumNumbers = 0;
+// while (i < arr.Length)
+// {
+//     sumNumbers += arr[i];
+//     i++;
+// }
+// Console.Write($"Сумма элементов массива равна: {sumNumbers}");
+
+// Попарная сумма элементов массива
+
+int[] number = { 56, 45, 22, 64, 33, 54, 80, 34 };
+int countPairs = number.Length / 2;
+int writeNumbers = 0;
+for (int j = 0; j < countPairs; j++)
 {
-    sumNumbers += arr[i];
-    i++;
+    writeNumbers = number[j * 2] + number[j * 2 + 1];
+    Console.WriteLine(writeNumbers);
 }
-Console.Write($"Сумма элементов массива равна: {sumNumbers}");
