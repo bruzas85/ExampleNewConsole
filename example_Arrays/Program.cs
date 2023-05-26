93078272f66aeeb63ec9fb2223a1ae0701143bbf
Erik Bruzhas <bruzas.85@mail.ru>
2023-05-26 19:14:30 +0300

    Сравнение элементов двух массивов

--- example_Arrays/Program.cs
+++ example_Arrays/Program.cs
@@ -1,16 +1,17 @@
 ﻿
-// Находим сумму элементов массива
-// int[] arr = { 6, 4, 3, 9, 12, 2, 8 };
-// int i = 0;
-// int sumNumbers = 0;
-// while (i < arr.Length)
-// {
-//     sumNumbers += arr[i];
-//     i++;
-// }
-// Console.Write($"Сумма элементов массива равна: {sumNumbers}");
+//Находим сумму элементов массива
 
-// Попарная сумма элементов массива
+int[] arr = { 6, 4, 3, 9, 12, 2, 8 };
+int i = 0;
+int sumNumbers = 0;
+while (i < arr.Length)
+{
+    sumNumbers += arr[i];
+    i++;
+}
+Console.Write($"Сумма элементов массива равна: {sumNumbers}");
+
+//Попарная сумма элементов массива
 
 int[] number = { 56, 45, 22, 64, 33, 54, 80, 34 };
 int countPairs = number.Length / 2;
@@ -19,4 +20,21 @@ for (int j = 0; j < countPairs; j++)
 {
     writeNumbers = number[j * 2] + number[j * 2 + 1];
     Console.WriteLine(writeNumbers);
+}
+
+// Сравнение элементов двух массивов
+
+int[] array1 = { 56, 45, 12, 98, 5, 24 };
+int[] array2 = { 34, 54, 60, 12, 3, 15 };
+for (int j = 0; j < array1.Length; j++)
+{
+    if (array1[j] > array2[j])
+    {
+        Console.WriteLine($"число массива array1 больше");
+    }
+    else
+    {
+        Console.WriteLine($"число массива array2 больше");
+    }
+
 }
