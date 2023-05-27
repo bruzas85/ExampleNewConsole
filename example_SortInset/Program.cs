02f64205e8c66ab0eddfd2e94e0d4b91308f82f7
Erik Bruzhas <bruzas.85@mail.ru>
2023-05-27 23:08:06 +0300

    Сортировка вставками

---
+++ example_SortInset/Program.cs
@@ -0,0 +1,20 @@
+﻿// Сортировка вставками
+
+int[] array = { 5, 10, 3, 16, 1, 32, 9, -4, 7, 45, 12 };
+int index;
+int currentNumber; // Текущее число
+for (int i = 0; i < array.Length; i++)
+{
+    index = i;
+    currentNumber = array[i];
+    while (index > 0 && currentNumber < array[index - 1])
+    {
+        array[index] = array[index - 1];
+        index--;
+    }
+    array[index] = currentNumber;
+}
+for (int j = 0; j < array.Length; j++)
+{
+    Console.WriteLine(array[j]);
+}
