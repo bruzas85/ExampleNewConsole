4b5098de309da9d8c0b52b29a90d404fbd7dbc37
Erik Bruzhas <bruzas.85@mail.ru>
2023-05-28 13:14:51 +0300

    Добавил некоторые коментарии и выводы

--- example_SortInset/Program.cs
+++ example_SortInset/Program.cs
@@ -1,14 +1,21 @@
 ﻿// Сортировка вставками
-
+Console.WriteLine("Выводим массив: ");
 int[] array = { 5, 10, 3, 16, 1, 32, 9, -4, 7, 45, 12 };
-int index;
+for (int j=0; j<array.Length; j ++)
+{
+    Console.Write($"{array[j]}, ");
+}
+Console.WriteLine();
+int index; // индекс числа, которое будем менять
 int currentNumber; // Текущее число
+Console.WriteLine("Выводим currentNumber: ");
 for (int i = 0; i < array.Length; i++)
 {
     index = i;
     currentNumber = array[i];
-    while (index > 0 && currentNumber < array[index - 1])
+    while (index > 0 && currentNumber < array[index - 1]) // идем в обратном порядке и вставляем число на свое место
     {
+        Console.Write($"{currentNumber}, ");
         array[index] = array[index - 1];
         index--;
     }
