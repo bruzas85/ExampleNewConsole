ae7ef4d24a3cead3dfcf16caa61ce1fbf3954eba
Erik Bruzhas <bruzas.85@mail.ru>
2023-05-30 00:02:30 +0300

    Add FillArray

---
+++ example_ArraysMethod_00/Program.cs
@@ -0,0 +1,14 @@
+﻿// Заполняем и выводим массив
+void FillArray(int[] array)
+{
+    var rand = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(-10, 11);
+    }
+    Console.Write($"{String.Join(", ", array)}");
+}
+
+int n = int.Parse(Console.ReadLine()!);
+int[] array = new int[n];
+FillArray(array);
