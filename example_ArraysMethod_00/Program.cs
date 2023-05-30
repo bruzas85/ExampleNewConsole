7489e32614e2762c6bb29daffe15d017eebabd3c
Erik Bruzhas <bruzas.85@mail.ru>
2023-05-30 22:56:20 +0300

    Add BubbleSort

--- example_ArraysMethod_00/Program.cs
+++ example_ArraysMethod_00/Program.cs
@@ -4,11 +4,34 @@ void FillArray(int[] array)
     var rand = new Random();
     for (int i = 0; i < array.Length; i++)
     {
-        array[i] = rand.Next(-10, 11);
+        array[i] = rand.Next(1, 100);
     }
     Console.Write($"{String.Join(", ", array)}");
 }
 
+void BubbleSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        for (int j = 0; j < array.Length - 1 - i; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+        // Console.Write($"{array[i]} ");
+    }
+
+    Console.WriteLine($"{String.Join(", ", array)}");
+}
+
+Console.WriteLine("Введите длину массива: ");
 int n = int.Parse(Console.ReadLine()!);
 int[] array = new int[n];
 FillArray(array);
+Console.WriteLine();
+BubbleSort(array);
+
